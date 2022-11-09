506781af41dd803871356718485f7b3552fb9707
Virkkunen <vrkknn@vrkknn.net>
2022-11-09 16:46:13 -0300

    req01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { getCategories } from './services/api';
 
 function App() {
-  getCategories();
   return (
     <div className="App">
       <header className="App-header">
--- src/services/api.js
+++ src/services/api.js
@@ -9,14 +9,34 @@ export async function getCategories() {
   // https://api.mercadolibre.com/sites/MLB/categories
   const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const categories = await fetchCategories.json();
-  console.log(categories);
+  // console.log(categories);
   return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  //
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
+  // /sites/$SITE_ID/search?category=$CATEGORY_ID
+  // MLB5672 = acessorios de carros (id de categoria)
+  // /sites/$SITE_ID/search?q=Motorola%20G6
+  if (!categoryId && !query) return;
+
+  if (categoryId.toLowerCase().includes('mlb')) {
+    const fetchProducts = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const productsResponse = await fetchProducts.json();
+    const products = productsResponse.results;
+    console.log(products);
+    return products;
+  }
+
+  const fetchQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const queryResponse = await fetchQuery.json();
+  console.log(queryResponse);
+  return queryResponse;
 }
 
-export async function getProductById() {
-  //
-}
+// export async function getProductById(itemId) {
+//   // https://api.mercadolibre.com/sites/MLB/items?id=$ITEM_ID1
+//   // MLB-2039042225
+//   const fetchItem = await fetch(`https://api.mercadolibre.com/sites/MLB/items?id=${itemId}`);
+//   const item = await fetchItem.json();
+//   console.log(item);
+// }
